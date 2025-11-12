1f1fb43ec93dae936b1f62b4f6280ee9fc2af536
Meow <tanishkkar.cs24@bmsce.ac.in>
2025-11-12 20:34:23 +0530

    TAn: Add login component, auth guard, and interceptor

--- backend/campussync-backend/src/api/middleware/file-upload.middleware.js
+++ backend/campussync-backend/src/api/middleware/file-upload.middleware.js
@@ -7,7 +7,7 @@ cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
-});
+}); 
 
 // Configure Storage
 const storage = new CloudinaryStorage({
